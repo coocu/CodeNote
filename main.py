5d6ea85f1e6bc2c0df25fb35446267441a5a6668
coocu <jxjphgt626@privaterlay.appleid.com>
2026-01-15 23:05:20 +0900

    server

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, PlainTextResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# 정적 파일
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+# 루트 페이지 (홈페이지)
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "title": "CodeNote"
+        }
+    )
+
+# Google Search Console 인증 파일
+@app.get("/googlexxxx.html")
+def google_verify():
+    return FileResponse("googlexxxx.html")
+
+# 헬스체크 (선택)
+@app.get("/health", response_class=PlainTextResponse)
+def health():
+    return "ok"
